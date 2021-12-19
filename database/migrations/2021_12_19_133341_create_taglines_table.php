cbef5838f1748bfd4b45f406d10b53439bb68465
Joe Dave <joshuadavian@gmail.com>
2021-12-19 20:55:58 +0700

    taglines migration

---
+++ database/migrations/2021_12_19_133341_create_taglines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaglinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taglines', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('service_id')->nullable();
+            $table->string('tagline')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taglines');
+    }
+}
