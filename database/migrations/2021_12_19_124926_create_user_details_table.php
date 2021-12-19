748fbb4d1389f0deb3c27124e309d3bf2e03761e
Joe Dave <joshuadavian@gmail.com>
2021-12-19 20:29:03 +0700

    user details migration

---
+++ database/migrations/2021_12_19_124926_create_user_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->longText('photo')->nullable();
+            $table->string('role')->nullable();
+            $table->string('contact_number')->nullable();
+            $table->longText('biography')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+}
