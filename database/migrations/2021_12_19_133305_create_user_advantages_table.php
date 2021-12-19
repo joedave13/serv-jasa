098b4a0d6f313bdc5c96bda61fe923eab96b04c9
Joe Dave <joshuadavian@gmail.com>
2021-12-19 20:55:39 +0700

    user advantages migration

---
+++ database/migrations/2021_12_19_133305_create_user_advantages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAdvantagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_advantages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('service_id')->nullable();
+            $table->string('advantage')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_advantages');
+    }
+}
