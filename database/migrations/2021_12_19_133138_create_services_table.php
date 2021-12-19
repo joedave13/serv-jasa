8d99c5e929c87b1ef47523d935ba71c813e7eee2
Joe Dave <joshuadavian@gmail.com>
2021-12-19 20:55:13 +0700

    services migration

---
+++ database/migrations/2021_12_19_133138_create_services_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('title')->nullable();
+            $table->longText('description')->nullable();
+            $table->integer('delivery_time')->nullable();
+            $table->integer('revision_limit')->nullable();
+            $table->integer('price')->default(0);
+            $table->longText('note')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
