5cbf497535c3745ba67b9716ad1c47d720e680fb
Joe Dave <joshuadavian@gmail.com>
2021-12-19 20:29:18 +0700

    user experiences migration

---
+++ database/migrations/2021_12_19_125724_create_user_experiences_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserExperiencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_experiences', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_detail_id')->nullable();
+            $table->longText('experience')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_experiences');
+    }
+}
