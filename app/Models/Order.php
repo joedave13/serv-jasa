e8d1e1247dc251930e969163a301205e8ebc5f5e
Joe Dave <joshuadavian@gmail.com>
2021-12-22 08:20:39 +0700

    order eloquent relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -27,4 +27,19 @@ class Order extends Model
         'updated_at',
         'deleted_at'
     ];
+
+    public function buyer()
+    {
+        return $this->belongsTo(User::class, 'buyer_id', 'id');
+    }
+
+    public function freelancer()
+    {
+        return $this->belongsTo(User::class, 'freelancer_id', 'id');
+    }
+
+    public function order_status()
+    {
+        return $this->belongsTo(OrderStatus::class, 'order_status_id', 'id');
+    }
 }
