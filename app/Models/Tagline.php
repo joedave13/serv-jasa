98a6c8ce7ac331e5179d0916de0ee7a361854e87
Joe Dave <joshuadavian@gmail.com>
2021-12-22 07:36:36 +0700

    tagline model

---
+++ app/Models/Tagline.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Tagline extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'service_id',
+        'tagline',
+    ];
+
+    protected $hidden = [];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
