aa62ecc5fa8dc23df5ea52b642fe833a1ae8c794
Joe Dave <joshuadavian@gmail.com>
2021-12-22 08:19:36 +0700

    tagline eloquent relationship

--- app/Models/Tagline.php
+++ app/Models/Tagline.php
@@ -22,4 +22,9 @@ class Tagline extends Model
         'updated_at',
         'deleted_at'
     ];
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class, 'service_id', 'id');
+    }
 }
