3d750b72f9e627594a8d595575e336391c8a0a65
Joe Dave <joshuadavian@gmail.com>
2021-12-22 07:21:36 +0700

    user experience model

---
+++ app/Models/UserExperience.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class UserExperience extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_detail_id',
+        'experience',
+    ];
+
+    protected $hidden = [];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
