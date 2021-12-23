77a5b530d5d8a0ae703314b26dafe9fdd458e371
Joe Dave <joshuadavian@gmail.com>
2021-12-23 15:52:45 +0700

    slicing layout blade for landing

---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @include('includes.landing.meta')
+
+    <title>@yield('title') | SERV</title>
+
+    @stack('before-style')
+    @include('includes.landing.style')
+    @stack('after-style')
+</head>
+
+<body class="antialiased">
+    <div class="relative">
+        @include('includes.landing.header')
+
+        {{-- @include('sweetalert::alert') --}}
+
+        @yield('content')
+
+        @include('includes.landing.footer')
+    </div>
+
+    @stack('before-script')
+    @include('includes.landing.script')
+    @stack('after-script')
+
+    @include('components.modal.login')
+    @include('components.modal.register')
+    @include('components.modal.register-success')
+</body>
+
+</html>
